3ea02f51707cedd5926c920bc92f5cd8e970f00d
Nobooooody <117302867+Nobooooody@users.noreply.github.com>
2026-05-23 00:51:21 +0800

    refactor: restructure flake.nix to match fcitx5-android pattern
    - Centralize SDK versions in makeExtensible attrset
    - Add overlays.default output
    - Use makeOverridable for shell with androidStudio/generateLocalProperties params
    - Add noAS devShell (w/o Android Studio)

--- flake.nix
+++ flake.nix
@@ -8,48 +8,69 @@
   };
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.android_sdk.accept_license = true;
           config.allowUnfree = true;
+          overlays = [ self.overlays.default ];
         };
-        androidSdk = pkgs.androidenv.composeAndroidPackages {
-          buildToolsVersions = [ "36.0.0" ];
-          platformVersions = [ "36" ];
-          includeNDK = false;
-          includeEmulator = false;
-          includeSources = false;
-        };
-      in
-      {
+      in {
         devShells = {
-          default = pkgs.mkShell {
-            buildInputs = [
-              androidSdk.androidsdk
-              pkgs.gettext
-              pkgs.python3
-              pkgs.jdk17
-            ];
-            ANDROID_SDK_ROOT = "${androidSdk.androidsdk}/libexec/android-sdk";
-            ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
-            JAVA_HOME = "${pkgs.jdk17}";
-            shellHook = ''
-              mkdir -p $HOME/.android
-              touch $HOME/.android/repositories.cfg
-              echo sdk.dir=$ANDROID_SDK_ROOT > local.properties
-            '';
+          default = pkgs.intent-modifier.sdk.shell;
+          noAS =
+            pkgs.intent-modifier.sdk.shell.override { androidStudio = null; };
+        };
+      }) // {
+        overlays.default = final: prev: {
+          intent-modifier = {
+            sdk = final.lib.makeExtensible (self: {
+
+              # Update versions here
+              buildToolsVersion = "36.0.0";
+              platformToolsVersion = "36.0.2";
+              platformVersion = "36";
+              ndkVersion = "28.0.13004108";
+
+              includeNDK = false;
+              androidComposition = final.androidenv.composeAndroidPackages {
+                inherit (self) platformToolsVersion ndkVersion includeNDK;
+                buildToolsVersions = [ self.buildToolsVersion ];
+                platformVersions = [ self.platformVersion ];
+                includeEmulator = false;
+                includeSources = false;
+              };
+
+              shell = final.lib.makeOverridable
+                ({ androidStudio, generateLocalProperties }:
+                  with final;
+                  with self;
+                  mkShell rec {
+                    buildInputs = [
+                      androidComposition.androidsdk
+                      gettext
+                      python3
+                      jdk17
+                      androidStudio
+                    ];
+                    ANDROID_SDK_ROOT =
+                      "${androidComposition.androidsdk}/libexec/android-sdk";
+                    ANDROID_HOME = ANDROID_SDK_ROOT;
+                    BUILD_TOOLS_VERSION = buildToolsVersion;
+                    GRADLE_OPTS =
+                      "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/${buildToolsVersion}/aapt2";
+                    JAVA_HOME = "${jdk17}";
+                    shellHook = lib.optionalString generateLocalProperties ''
+                      echo sdk.dir=$ANDROID_SDK_ROOT > local.properties
+                    '';
+                  }) {
+                    androidStudio = final.androidStudioPackages.stable;
+                    generateLocalProperties = true;
+                  };
+            });
           };
         };
-      }
-    );
+      };
 }
