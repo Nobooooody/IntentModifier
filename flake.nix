062fe34983a230f17f17e2c3092bc6ba3baabbb9
Nobooooody <117302867+Nobooooody@users.noreply.github.com>
2026-09-03 01:58:33 +0800

    chore: update nixpkgs with nix-config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Dev shell flake for IntentModifier";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=b5aa0fbd538984f6e3d201be0005b4463d8b09f8";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=1559d3daa3ecc813a650b79375ea61b6741b8746";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
